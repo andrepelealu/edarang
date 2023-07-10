16bfb9ef67f1dd9703f86c105e3dd84c5bc09ddd
Andre Aditya Pelealu <andre02.9d@gmail.com>
2023-07-11 03:39:54 +0700

    change css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
-import "../styles/tailwind.css";
-import "../styles/slick.css";
+import Head from 'next/head';
+import '../styles/tailwind.css';
+import '../styles/slick.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.tailwindcss.com"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/react-slick/0.29.0/react-slick.min.js"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
